c626e78ff5562c86262eb378dd9f7f35d4abfa99
Sanjaykumar Kamble <kamblesanjaykumar.n@gmail.com>
2022-07-12 15:45:44 +0530

    Footer added

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer'
 
 export default function Home() {
   return (
     <div>
       <Navbar></Navbar>
       <div>Welcome !</div>
+      <Footer></Footer>
     </div>
   )
 }
