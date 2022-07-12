25fca61c455e805fd8ddc373acbe572919c3a10d
Sanjaykumar Kamble <kamblesanjaykumar.n@gmail.com>
2022-07-12 15:51:48 +0530

    footer added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
 import Navbar from '../components/Navbar/Navbar'
 import Footer from '../components/Footer/Footer'
 
