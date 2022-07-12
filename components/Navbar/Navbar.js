3bb195a7ad554a4f6ee72eed3a0647beb84e2ed8
Sanjaykumar Kamble <kamblesanjaykumar.n@gmail.com>
2022-07-12 15:31:03 +0530

    initial commit

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>Logo</div>
+        <div>Register</div>
+        <div>Login</div>
+    </div>
+  )
+}
+
+export default Navbar
